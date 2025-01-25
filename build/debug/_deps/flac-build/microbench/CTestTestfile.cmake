7dab3522f0da67273fd4e73de0502028194f72a2
Benjamin <148866497+walliscode@users.noreply.github.com>
2025-01-25 22:13:03 +0000

    Delete build directory

--- build/debug/_deps/flac-build/microbench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/benja/source/repos/walliscode/Cuttlefish/build/debug/_deps/flac-src/microbench
-# Build directory: C:/Users/benja/source/repos/walliscode/Cuttlefish/build/debug/_deps/flac-build/microbench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
